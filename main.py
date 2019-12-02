836140f10b768d6f4b3f186b255721ff89df7e3c
Adam Jensen <adam.jensen@keethealth.com>
2019-12-02 09:11:36 -0700

    2 done

--- main.py
+++ main.py
@@ -1,43 +1,201 @@
 import sys
 
-testData = {14: 2, 1969: 966, 100756: 50346}
-fuelCalc = lambda x: int(int(x) / 3) - 2
+testData = [
+    {
+        "values": [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50],
+        "answer": [3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50],
+    },
+    {"values": [1, 0, 0, 0, 99], "answer": [2, 0, 0, 0, 99]},
+    {"values": [2, 3, 0, 3, 99], "answer": [2, 3, 0, 6, 99]},
+    {"values": [2, 4, 4, 5, 99, 0], "answer": [2, 4, 4, 5, 99, 9801]},
+    {"values": [1, 1, 1, 4, 99, 5, 6, 0, 99], "answer": [30, 1, 1, 4, 2, 5, 6, 0, 99]},
+]
+realData = [
+    1,
+    12,
+    2,
+    3,
+    1,
+    1,
+    2,
+    3,
+    1,
+    3,
+    4,
+    3,
+    1,
+    5,
+    0,
+    3,
+    2,
+    10,
+    1,
+    19,
+    1,
+    6,
+    19,
+    23,
+    1,
+    23,
+    13,
+    27,
+    2,
+    6,
+    27,
+    31,
+    1,
+    5,
+    31,
+    35,
+    2,
+    10,
+    35,
+    39,
+    1,
+    6,
+    39,
+    43,
+    1,
+    13,
+    43,
+    47,
+    2,
+    47,
+    6,
+    51,
+    1,
+    51,
+    5,
+    55,
+    1,
+    55,
+    6,
+    59,
+    2,
+    59,
+    10,
+    63,
+    1,
+    63,
+    6,
+    67,
+    2,
+    67,
+    10,
+    71,
+    1,
+    71,
+    9,
+    75,
+    2,
+    75,
+    10,
+    79,
+    1,
+    79,
+    5,
+    83,
+    2,
+    10,
+    83,
+    87,
+    1,
+    87,
+    6,
+    91,
+    2,
+    9,
+    91,
+    95,
+    1,
+    95,
+    5,
+    99,
+    1,
+    5,
+    99,
+    103,
+    1,
+    103,
+    10,
+    107,
+    1,
+    9,
+    107,
+    111,
+    1,
+    6,
+    111,
+    115,
+    1,
+    115,
+    5,
+    119,
+    1,
+    10,
+    119,
+    123,
+    2,
+    6,
+    123,
+    127,
+    2,
+    127,
+    6,
+    131,
+    1,
+    131,
+    2,
+    135,
+    1,
+    10,
+    135,
+    0,
+    99,
+    2,
+    0,
+    14,
+    0,
+]
 
 
-def getData():
-    with open(sys.argv[1]) as f:
-        weights = f.readlines()
-    return weights
-
-
-def moduleFuel(weights):
-    fuel = 0
-    for w in weights:
-        f = fuelCalc(w)
-        g = fuelFuel(f) + f
-        fuel += g
-    return fuel
-
-
-def fuelFuel(fuel):
-    f = fuelCalc(fuel)
-    g = 0
-    if f > 0:
-        g = fuelFuel(f)
-        # print(f"Called with {fuel} got {f} + {g}")
-        return f + g
+def opCode(data, a):
+    if data[a] == 99:
+        # print("Done")
+        return False
     else:
-        # print(f"Ending with {f}")
-        return 0
+        b = data[a + 1]
+        c = data[a + 2]
+        d = data[a + 3]
+    # print(f"Addrs {b}, {c}, {d}, values {data[b]}, {data[c]}, {data[d]}")
+    if data[a] == 1:
+        data[d] = data[b] + data[c]
+    elif data[a] == 2:
+        data[d] = data[b] * data[c]
+    else:
+        print("Error! Invalid OpCode")
+    # print(f"New value: {data[d]}")
+    return True
 
 
-if __name__ == "__main__":
-    for value, answer in testData.items():
-        result = moduleFuel([value])
-        if result == answer:
-            print("Passed")
-        else:
-            print(f"Failed; value {value} returned {result} but expected {answer}")
-            sys.exit()
-    print(moduleFuel(getData()))
+for test in testData:
+    sp = 0
+    while opCode(test["values"], sp):
+        sp += 4
+    if test["values"] != test["answer"]:
+        print("Failed!")
+        print(test)
+        sys.exit()
+    print("Passed")
+
+for i in range(99):
+    for j in range(99):
+        data = realData.copy()
+        data[1] = i
+        data[2] = j
+        ip = 0
+        while opCode(data, ip):
+            ip += 4
+        if data[0] == 19690720:
+            print(f"{i}{j}")
 
