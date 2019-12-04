4238538c57a243bc8cb5f413388271f6c466a631
Adam Jensen <adam.jensen@keethealth.com>
2019-12-03 23:24:36 -0700

    4 complete

--- done/4.py
+++ done/4.py
@@ -1,7 +1,14 @@
+import sys
+
 testData = [
-    {"combo": [1, 1, 1, 1, 1, 1], "valid": True},
+    {"combo": [1, 1, 1, 1, 1, 1], "valid": False},
     {"combo": [1, 2, 3, 4, 5, 6], "valid": False},
     {"combo": [1, 2, 3, 4, 5, 0], "valid": False},
+    {"combo": [1, 1, 2, 2, 3, 3], "valid": True},
+    {"combo": [1, 2, 3, 4, 4, 4], "valid": False},
+    {"combo": [1, 1, 1, 1, 4, 4], "valid": True},
+    {"combo": [7, 7, 7, 8, 8, 9], "valid": True},
+    {"combo": [7, 8, 8, 9, 9, 9], "valid": True},
 ]
 
 low = [3, 5, 5, 5, 5, 5]  # 353096
@@ -18,11 +25,21 @@ def valid(combo):
         return False
     if len(set(combo)) == 6:  # no duplicates
         return False
+    runs = []
+    run = 0
     last = 0
     for d in combo:
         if d < last:
             return False
+        if d == last:
+            run += 1
+        else:
+            runs.append(run)
+            run = 0
         last = d
+    runs.append(run)
+    if 1 not in runs:
+        return False
     return True
 
 
@@ -41,7 +58,8 @@ for test in testData:
     if out == test["valid"]:
         print("Passed")
     else:
-        print("Failed")
+        print(f"Failed: {test['combo']}")
+        sys.exit(1)
 
 combos = 0
 while low[0] < 8:
